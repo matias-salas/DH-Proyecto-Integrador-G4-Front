c36f6d887ee305df6a215192957c1cd6d5d5bb26
Conrado Els <conradoels@gmail.com>
2023-10-24 14:22:02 -0300

    agrego footer y array de objetos para simular un mapeo api

--- digital-car/src/components/layout/Footer/Footer.jsx
+++ digital-car/src/components/layout/Footer/Footer.jsx
@@ -5,7 +5,34 @@ const Footer = () => {
     <>
       <div className="footer">
         <div>
-          <img src="./public/images/logoFooter.png" alt="logo" className="logo-footer" />
+          <img
+            src="./public/images/logoFooter.png"
+            alt="logo"
+            className="logo-footer"
+          />
+          <span>Copyright © 2023</span>
+        </div>
+        <div className="social-icon">
+          <img
+            src="./images/ico-facebook.png"
+            alt="facebook"
+            className="icon-media"
+          />
+          <img
+            src="./images/ico-instagram.png"
+            alt="instagram"
+            className="icon-media"
+          />
+          <img
+            src="./images/ico-tiktok.png"
+            alt="tiktok"
+            className="icon-media"
+          />
+          <img
+            src="./images/ico-whatsapp.png"
+            alt="whatsapp"
+            className="icon-media"
+          />
         </div>
       </div>
     </>
---
+++ digital-car/src/productsMock.js
@@ -0,0 +1,82 @@
+export const products = [
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+    {
+        title: "Fiat",
+        model: "Cronos",
+        type: "Sedan",
+        price: 3000,
+        year: 2019,
+        img: "https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjc5OTYwL0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNjk2NDM4NDU0MzEzLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0="
+    },
+]
