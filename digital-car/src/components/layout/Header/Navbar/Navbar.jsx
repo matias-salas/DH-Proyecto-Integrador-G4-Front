1e0f3da401a2871106ac2687afaaf0338acf866f
facu893 <facundoavilaceballos@gmail.com>
2023-10-25 21:28:15 -0300

    Modifique navbar en base a criterios de aceptacion faltantes

--- digital-car/src/components/layout/Header/Navbar/Navbar.jsx
+++ digital-car/src/components/layout/Header/Navbar/Navbar.jsx
@@ -1,12 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import SearchAppBar from "../../../common/Search/SearchAppBar";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="navbar-container">
-      <div className="navbar">Navbar</div>
+    <header className="navbar-container">
+      <div className="navbar">
+        <div className="left-content">
+          <Link to="/" className="logo-link">
+            <img src="./images/logoDC.png" alt="Logo" className="logo-image" /> 
+            <span className="company-slogan">Lema de la empresa</span>
+          </Link>
+        </div>
+        <div className="right-content">
+          <button className="nav-button">Crear cuenta</button>
+          <button className="nav-button">Iniciar sesión</button>
+        </div>
+      </div>
       <SearchAppBar />
-    </div>
+    </header>
   );
 };
 
