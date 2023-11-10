5f48db97c975eee3c9c61e1c26f9c1fb9f127d94
Conrado Els <conradoels@gmail.com>
2023-11-10 04:34:46 -0300

    menu burger para filtrar por categoria

--- digital-car/src/components/common/Search/SearchAppBar.jsx
+++ digital-car/src/components/common/Search/SearchAppBar.jsx
@@ -1,61 +1,85 @@
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+import Drawer from "@mui/material/Drawer";
+import { Link } from "react-router-dom";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
       },
     },
   },
 }));
 
 const SearchAppBar = () => {
+  const [state, setState] = useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{'backgroundColor':'var(--color-four)'}}>
-        <Toolbar >
+      <AppBar
+        position="static"
+        style={{ backgroundColor: "var(--color-four)" }}
+      >
+        <Toolbar>
           <IconButton
+            onClick={toggleDrawer("left", true)}
             size="large"
             edge="start"
             color="inherit"
@@ -64,28 +88,58 @@ const SearchAppBar = () => {
           >
             <MenuIcon />
           </IconButton>
+          <Drawer
+            anchor={"left"}
+            open={state["left"]}
+            onClose={toggleDrawer("left", false)}
+          >
+            <Link to="/type/suv" style={{ textDecoration: "none" , color: "inherit"}}>
+              Suv
+            </Link>
+            <Link to="/type/pickup" style={{ textDecoration: "none", color: "inherit" }}>
+              Pick Up
+            </Link>
+            <Link to="/type/van" style={{ textDecoration: "none" , color: "inherit"}}>
+              Van
+            </Link>
+            <Link to="/type/coupe" style={{ textDecoration: "none", color: "inherit" }}>
+              Coupe
+            </Link>
+            <Link to="/type/sedan" style={{ textDecoration: "none" , color: "inherit"}}>
+              Sedan
+            </Link>
+            <Link to="/type/minivan" style={{ textDecoration: "none" , color: "inherit"}}>
+              Minivan
+            </Link>
+            <Link to="/type/hatchback" style={{ textDecoration: "none" , color: "inherit"}}>
+              Hatchback
+            </Link>
+            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+              Ver Todos
+            </Link>
+          </Drawer>
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
             Categorias
           </Typography>
-         
+
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
             </SearchIconWrapper>
             <StyledInputBase
               placeholder="Marca, modelo, tipo..."
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
             />
           </Search>
         </Toolbar>
       </AppBar>
     </Box>
   );
-}
+};
 
-export default SearchAppBar
+export default SearchAppBar;
