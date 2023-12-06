e043b01f3a792cfa6ce0b8e5bc9338a362621cf4
santimp <santimeson@gmail.com>
2023-12-06 09:21:21 -0300

    estilos de detalles de reserva

--- digital-car/src/components/pages/VerDetalles/DetallesReserva.jsx
+++ digital-car/src/components/pages/VerDetalles/DetallesReserva.jsx
@@ -5,9 +5,36 @@ import { products } from "../../../productsMock";
 const DetallesReserva = () => {
   return (
     <div className='reserva-card'>
+
         <h1>Detalles de la reserva</h1>
-       
-        
+
+        <div className="caracteristicas">
+
+        <div className="card-details">
+
+        <div className='car-details'>
+
+       <h2>nombre del auto</h2>
+       <h3>modelo</h3>
+       <h4>descripcion</h4>
+       <img src="https://images.prd.kavak.io/eyJidWNrZXQiOiJrYXZhay1pbWFnZXMiLCJrZXkiOiJpbWFnZXMvMjg2ODg5L0VYVEVSSU9SLWZyb250U2lkZVBpbG90TmVhci0xNzAwNTkwOTQ3NTEyLmpwZWciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjgxMCwiaGVpZ2h0Ijo0NjV9fX0=" alt="" />
+       <h3>fecha seleccionada</h3>
+
+        </div>
+
+          <div className="datos-usuario">
+
+            <h4>username:</h4>
+            <h4>mail:</h4>
+            <h4>cellphone:</h4>
+
+        </div>
+
+
+          </div>
+         </div>
+
+        <button>Confirmar reserva</button>
     </div>
    
   )
